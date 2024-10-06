ef5d9b9f37c6e1a8a4a48568b151c8ba36de4c25
Emertat <Emertat@RAITY>
2024-10-06 21:48:41 +0330

    Add project files.

---
+++ NucleotideCount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class NucleotideCount
+{
+    public static IDictionary<char, int> Count(string sequence)
+    {
+        // Create a dictionary to store the count of each nucleotide
+        var nucleotideDictionary = new Dictionary<char, int>
+        {
+            {'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}
+        };
+
+        // Iterate through each character in the input sequence
+        foreach (char c in sequence)
+        {
+            // Check if the character is a valid nucleotide (A, C, G, or T)
+            if (!nucleotideDictionary.ContainsKey(c))
+            {
+                // If an invalid character is found, throw an error
+                throw new ArgumentException($"Invalid character in DNA sequence: {c}");
+               
+            }
+
+            // Increment the count of the corresponding nucleotide
+            nucleotideDictionary[c]++;
+        }
+
+        // Return the final dictionary with the counts
+        return nucleotideDictionary;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace nucleotide_count;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+       
+        foreach(var item in NucleotideCount.Count("GATTACA"))
+        {
+            Console.Write( item.Key + " : "+ item.Value +",");
+        }
+       
+    }
+}
